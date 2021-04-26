e47deb09ab5f847c66062a919ec58860499d7090
Kaan Divringi <kdivringi@gmail.com>
2021-04-25 19:59:09 -0700

    Looking very passable right now

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -4,26 +4,11 @@
 
 <style>
 	nav {
-		border-bottom: 1px solid rgba(255,62,0,0.1);
-		font-weight: 300;
-		padding: 0 1em;
+		@apply border-red-100 border-b font-light px-4 flow-root;
 	}
 
 	ul {
-		margin: 0;
-		padding: 0;
-	}
-
-	/* clearfix */
-	ul::after {
-		content: '';
-		display: block;
-		clear: both;
-	}
-
-	li {
-		display: block;
-		float: left;
+		@apply flex;
 	}
 
 	[aria-current] {
@@ -42,17 +27,15 @@
 	}
 
 	a {
-		text-decoration: none;
-		padding: 1em 0.5em;
-		display: block;
+		@apply block py-4 px-2;
 	}
 </style>
 
 <nav>
 	<ul>
 		<li><a aria-current="{segment === undefined ? 'page' : undefined}" href=".">home</a></li>
-		<li><a aria-current="{segment === 'about' ? 'page' : undefined}" href="about">about</a></li>
-		<li><a aria-current="{segment ==='portfolio' ? 'page' : undefined }" href="portfolio">portfolio</a></li>
+		<!-- <li><a aria-current="{segment === 'about' ? 'page' : undefined}" href="about">about</a></li> -->
+		<!-- <li><a aria-current="{segment ==='portfolio' ? 'page' : undefined }" href="portfolio">portfolio</a></li> -->
 		<li><a aria-current="{segment ==='feed' ? 'page' : undefined }" href="feed">feed</a></li>
 	</ul>
 </nav>
--- src/routes/feed/index.svelte
+++ src/routes/feed/index.svelte
@@ -9,7 +9,20 @@
 	export let posts;
 </script>
 
-<h1>Dis da feed</h1>
+<style>
+    h1 {
+        @apply text-2xl mb-4 font-semibold tracking-wide;
+    }
+    ul {
+        list-style-type: disc;
+        list-style-position: inside;
+    }
+    a {
+        @apply text-blue-500 font-semibold underline hover:text-blue-700;
+    }
+</style>
+
+<h1>As I do things they'll show up here</h1>
 
 <ul>
     {#each posts as post }
