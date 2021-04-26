626b8ec5f25bb2d993789f6a7725f1b346a6b798
Kaan Divringi <kdivringi@gmail.com>
2021-04-26 06:53:25 -0700

    Removing incomplete post from feed

--- src/routes/feed/_posts.js
+++ src/routes/feed/_posts.js
@@ -4,9 +4,9 @@ export const listing = [
     date: "2021-4-14",
     title: "Obligatory blog stack post",
   },
-  {
-    slug: "making-sense-of-tailwind-in-svelte",
-    date: "2021-4-25",
-    title: "Making sense of tailwind in svelte",
-  },
+  // {
+  //   slug: "making-sense-of-tailwind-in-svelte",
+  //   date: "2021-4-25",
+  //   title: "Making sense of tailwind in svelte",
+  // },
 ];
